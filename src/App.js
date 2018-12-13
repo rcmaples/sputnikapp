caae1513892285a060fd0a3eaca1014e154f2276
RC Maples <rcmaples@users.noreply.github.com>
2018-12-13 13:24:11 -0500

    Adding preliminary loader gif/component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import loaderGif from './SatelliteLoader@2x.gif';
 import './App.css';
-
+import Loader from './components/Loader';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{ backgroundcolor: 'white' }}>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={loaderGif} className="App-logo loader" alt="logo" />
         </header>
       </div>
     );
---
+++ src/components/Loader.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import loaderGif from '../SatelliteLoader@2x.gif';
+
+const Loader = () => (
+  <img src={loaderGif} alt="loading.." style={{ width: '60px' }} />
+);
+
+export default Loader;
