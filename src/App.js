ec7ae7065b3d4f960803dd20cdddc52bb0677e12
RC Maples <rcmaples@users.noreply.github.com>
2018-12-13 22:50:01 -0500

    Changed out gif to svg/css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import loaderGif from './SatelliteLoader@2x.gif';
 import './App.css';
 import Loader from './components/Loader';
 class App extends Component {
@@ -8,7 +6,7 @@ class App extends Component {
     return (
       <div className="App" style={{ backgroundcolor: 'white' }}>
         <header className="App-header">
-          <img src={loaderGif} className="App-logo loader" alt="logo" />
+          <Loader />
         </header>
       </div>
     );
