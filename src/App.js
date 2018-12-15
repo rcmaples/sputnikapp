bd81897be36d132cd164ed09ab9ed5ef1642d36b
RC Maples <rcmaples@users.noreply.github.com>
2018-12-15 16:42:23 -0500

    stying header and loader.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './styles/styles.scss';
 import Header from './components/Header';
-// import Loader from './components/Loader';
+import Loader from './components/Loader';
 class App extends Component {
   render() {
     return (
       <div className="App" style={{ backgroundcolor: 'white' }}>
         <Header />
+        <main>
+          <Loader />
+        </main>
       </div>
     );
   }
