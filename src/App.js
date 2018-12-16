2797c30fb6ffc11b9f5d3d896877371e0a7c5c70
RC Maples <rcmaples@users.noreply.github.com>
2018-12-16 14:30:52 -0500

    Adding new components.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import './styles/styles.scss';
 import Header from './components/Header';
-import FeedComponent from './components/FeedComponent';
+// import { LoginForm } from './components/LoginForm';
+import { RegistrationForm } from './components/RegisterForm';
+// import FeedComponent from './components/FeedComponent';
+
 class App extends Component {
   render() {
     return (
       <div className="App" style={{ backgroundcolor: 'white' }}>
         <Header />
         <main>
-          <FeedComponent type="following" />
+          <RegistrationForm />
+          {/* <FeedComponent type="following" />
           <FeedComponent type="watching" />
           <FeedComponent type="starred" />
           <FeedComponent type="repos" />
-          <FeedComponent type="popular" />
+          <FeedComponent type="popular" /> */}
         </main>
       </div>
     );
