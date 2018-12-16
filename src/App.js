5ddb63a0616b921d892f2715ded7515374a67618
RC Maples <rcmaples@users.noreply.github.com>
2018-12-16 15:25:51 -0500

    Added routing and all pages.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,4 @@
-import React, { Component } from 'react';
-import './styles/styles.scss';
-import Header from './components/Header';
-// import { LoginForm } from './components/LoginForm';
-import { RegistrationForm } from './components/RegisterForm';
-// import FeedComponent from './components/FeedComponent';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App" style={{ backgroundcolor: 'white' }}>
-        <Header />
-        <main>
-          <RegistrationForm />
-          {/* <FeedComponent type="following" />
-          <FeedComponent type="watching" />
-          <FeedComponent type="starred" />
-          <FeedComponent type="repos" />
-          <FeedComponent type="popular" /> */}
-        </main>
-      </div>
-    );
-  }
-}
-
+const App = () => <AppRouter />;
 export default App;
---
+++ src/components/LandingPage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const LandingPage = () => <p>Landed</p>;
+
+export default LandingPage;
