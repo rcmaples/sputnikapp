0b774ecd38fec6c9aca6388838db62e8cc192120
RC Maples <rcmaples@users.noreply.github.com>
2018-12-17 16:04:20 -0500

    Fixing up the 404.

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import lostInSpace from '../images/404-astronaut.svg';
 
 const NotFoundPage = () => (
-  <div>
-    404 - <Link to="/">Go home</Link>
+  <div className="notFoundContainer">
+    <h1 className="hero">404</h1>
+    <img src={lostInSpace} alt="Lost in Space" className="hover" />
+    <p className="text">Uh oh, this page seems to be lost in space...</p>
+    <p className="text">
+      Maybe you should go back <a href="/">home</a>
+    </p>
   </div>
 );
 
