4d33985938dacf90df7b03af1cdeb0c968dd4a75
RC Maples <rcmaples@users.noreply.github.com>
2018-12-16 20:11:55 -0500

    landing page wireframe.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,5 +1,22 @@
+// import React, { Component } from 'react';
 import React from 'react';
 
-const LandingPage = () => <p>Landed</p>;
+import iconLogo from '../images/spunik-mark.svg';
+
+import Loader from './Loader';
+
+const LandingPage = props => {
+  return (
+    <div className="landingPage">
+      <div className="feedComponentHeader">
+        <img src={iconLogo} alt="Landing Page Icon" />
+        <span className="feedComponentTitle">What's this about?</span>
+      </div>
+      <div className="landingPageContainer">
+        <Loader />
+      </div>
+    </div>
+  );
+};
 
 export default LandingPage;
