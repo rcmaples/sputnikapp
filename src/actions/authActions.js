1891c5f6dd0e1661dc29e2490f9be3be58a57cb7
RC Maples <rcmaples@users.noreply.github.com>
2018-12-29 16:59:41 -0500

    Added redux store.

---
+++ src/actions/authActions.js
@@ -0,0 +1,84 @@
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+
+import setAuthToken from '../utils/setAuthToken';
+import {
+  GET_ERRORS,
+  GET_CURRENT_USER,
+  SET_CURRENT_USER,
+  USER_LOADING
+} from './types';
+
+// Registration
+export const registerUser = (userData, history) => dispatch => {
+  axios
+    .post('/api/users/register', userData)
+    .then(res => history.push('/login'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+
+// Login and get user token
+export const loginUser = userData => dispatch => {
+  axios
+    .post('api/users/login', userData)
+    .then(res => {
+      const { token } = res.data;
+      localStorage.setItem('jwtToken', token);
+      setAuthToken(token);
+      const decoded = jwt_decode(token);
+      dispatch(setCurrentUser(decoded))
+    })
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    })
+}
+
+// set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
+}
+
+// Get user
+export const getCurrentUser = () => dispatch => {
+  dispatch(setUserLoaded());
+  axios
+    .get('/api/user/currentuser')
+    .then(res =>
+      dispatch({
+        type: GET_CURRENT_USER,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+}
+
+// load user
+export const setUserLoading = () => {
+  return {
+    type: USER_LOADING
+  };
+};
+
+// log out user
+export const logoutUser = () => dispatch => {
+    localStorage.removeItem('jwtToken');
+    setAuthToken(false);
+    dispatch(setCurrentUser({}));
+}
