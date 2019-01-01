d3d2203b54fc106135ef140f5ced0c1a91695d52
RC Maples <rcmaples@users.noreply.github.com>
2018-12-31 19:26:32 -0500

    Adding support for Travis/Redux-DevsTools

--- src/store.js
+++ src/store.js
@@ -1,17 +1,30 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
 const middleware = [thunk];
 
+const composeEnhancers = composeWithDevTools({
+  features: {
+    pause: true, // start/pause recording of dispatched actions
+    lock: true, // lock/unlock dispatching actions and side effects
+    persist: true, // persist states on page reloading
+    export: true, // export history of actions in a file
+    import: 'custom', // import history of actions from a file
+    jump: true, // jump back and forth (time travelling)
+    skip: true, // skip (cancel) actions
+    reorder: true, // drag and drop actions in the history list
+    dispatch: true, // dispatch custom actions or action creators
+    test: true // generate tests for the selected actions
+  }
+});
+
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 export default store;
