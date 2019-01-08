5ef5aa87e5c37001112de6522bc0b78e5e65faab
RC Maples <rcmaples@users.noreply.github.com>
2019-01-07 21:20:44 -0500

    Adding Followers Action and Initial API Calls.

--- src/actions/follwerActions.js
+++ src/actions/follwerActions.js
@@ -1,1 +1,11 @@
-import axios from 'axios';
+import github from '../api/github';
+
+export const getFollowList = token => async dispatch => {
+  const response = await github.get('/user/following', {
+    headers: { Authorization: `Bearer ${token}` }
+  });
+  dispatch({
+    type: 'GET_FOLLOWER_LIST',
+    payload: response.data
+  });
+};
