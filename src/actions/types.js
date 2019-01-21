341df1537e4c2cd1cceac8800c2a964f3d14fa8e
RC Maples <rcmaples@users.noreply.github.com>
2019-01-21 15:13:21 -0500

    Added new type.

--- src/actions/types.js
+++ src/actions/types.js
@@ -7,3 +7,4 @@ export const GET_GITHUB_TOKEN = 'GET_GITHUB_TOKEN';
 export const GET_FOLLOWER_LIST = 'GET_FOLLOWER_LIST';
 export const GET_FOLLOWER_ACT = 'GET_FOLLOWER_ACT';
 export const SET_URLS = 'SET_URLS';
+export const GET_REPOS_LIST = 'GET_REPOS_LIST';
